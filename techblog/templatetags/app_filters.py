731f3cfda35f16e5e84f96c2958019c8d12b89e3
smd <smdalton@ucsc.edu>
2018-08-16 14:08:44 -0700

    made small changes

--- techblog/templatetags/app_filters.py
+++ techblog/templatetags/app_filters.py
@@ -2,10 +2,35 @@ from django import template
 # this is the dict that maps categories choices names and icons together,
 # centralized in the management section
 from techblog.management.commands.structures import choices_map
+"""
+needed for pretty markdown rendering courtesy of
+https://www.ignoredbydinosaurs.com/posts/275-easy-markdown-and-syntax-highlighting-django
+v v v v v v v v
+"""
+import mistune
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
+
+
 register = template.Library()
 
 # choices map is a tuple of an icon, and it's specified color, the color is appended to text in template
 
+
+class HighlightRenderer(mistune.Renderer):
+    def block_code(self, code, lang):
+        if not lang:
+            return '\n<pre><code>%s</code></pre>\n' % mistune.escape(code)
+        lexer = get_lexer_by_name(lang, stripall=True)
+        formatter = HtmlFormatter()
+        return highlight(code, lexer, formatter)
+
+
+@register.filter()
+def markdown(value):
+    markdown = mistune.Markdown()
+    return markdown(value)
 
 @register.filter()
 def map_category_to_icon(category):
