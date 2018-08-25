ffd09873af2ded801f3dfa60c51ed46bfd340eb9
smd <smdalton@ucsc.edu>
2018-08-25 00:11:15 -0600

    again

--- blog/settings.py
+++ blog/settings.py
@@ -94,11 +94,11 @@ SECRET_KEY = uuid.hex
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['RDS_DB_NAME'],
-        'USER': os.environ['RDS_USERNAME'],
-        'PASSWORD': os.environ['RDS_PASSWORD'],
-        'HOST': os.environ['RDS_HOSTNAME'],
-        'PORT': os.environ['RDS_PORT'],
+        'NAME': 'ebdb',
+        'USER': 'blogdoggy',
+        'PASSWORD': 'X4A-vXu-g23-kEM',
+        'HOST': 'aa1clg23u1gz4pm.cl5rpdi2w2we.us-west-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 # else:
